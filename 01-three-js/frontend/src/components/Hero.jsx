43e1139024a8f818b1bdc65fe0095374a96a0a3c
Lazleo <l.belkovits@gmail.com>
2024-03-26 22:54:19 +0100

    add isometric cube

--- 01-three-js/frontend/src/components/Hero.jsx
+++ 01-three-js/frontend/src/components/Hero.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Canvas } from "@react-three/fiber";
 
 export default function Hero() {
   return (
-    <div className='text-green-600 font-bold text-lg'>Hero</div>
-  )
+    <div className=" bg-blue-700 h-screen w-full">
+      <Canvas camera={{ position: [5, 5, 5], fov: 45 }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <mesh position={[0, 0, 0]}>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
