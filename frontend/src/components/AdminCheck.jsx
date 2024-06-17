7cffac90b06fbf558867252ad88d62f388569721
Lazleo <l.belkovits@gmail.com>
2024-06-17 16:33:59 +0200

    add layout structure

---
+++ frontend/src/components/AdminCheck.jsx
@@ -0,0 +1,5 @@
+export default function AdminCheck() {
+  return (
+    <div>AdminCheck</div>
+  )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import MainLayout from "./layouts/MainLayout";
+import ErrorElement from "./routes/ErrorElement";
+import Home from "./routes/Home";
+import Projects from "./routes/Projects";
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import AdminLayout from "./layouts/AdminLayout";
+import Admin from "./routes/Admin";
+// import AdminCheck from "./components/AdminCheck";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorElement />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "projects",
+        element: <Projects />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "/admin/",
+    element: (
+      // <AdminCheckCheck>
+        <AdminLayout />
+      // </AdminCheck>
+    ),
+    errorElement: <ErrorElement />,
+    children: [
+      {
+        path: "",
+        element: <Admin />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
