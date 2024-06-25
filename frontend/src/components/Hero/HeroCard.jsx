ecaab2857d5ce575a6981af329728748ee1a7667
Lazleo <l.belkovits@gmail.com>
2024-06-25 20:22:58 +0200

    edit hero layout

--- frontend/src/components/Hero/HeroCard.jsx
+++ frontend/src/components/Hero/HeroCard.jsx
@@ -2,7 +2,7 @@ import PrimaryButton from "../PrimaryButton";
 
 export default function HeroCard() {
   return (
-    <div className="absolute top-[12%] left-[10%] flex flex-col items-center border-2 border-gray-600 bg-primary/15 backdrop-blur-md shadow-glow rounded-3xl p-8">
+    <div className="absolute top-[40%] left-[33%] -translate-x-[50%] -translate-y-[50%] z-10 flex flex-col items-center border-2 border-gray-600 bg-primary/15 backdrop-blur-md shadow-glow rounded-3xl p-8">
       <div className="w-full">
         <div className="flex gap-4 font-primary font-extrabold text-4xl pt-4">
           <div>{`Hi! I'm`}</div>
