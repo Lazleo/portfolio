3b228085beda36380f02d9e60bb0e552b39f5312
Lazleo <l.belkovits@gmail.com>
2024-06-25 16:05:00 +0200

    add fonts and colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5BF4BD",
+        accent: "#1E2973",
+        neutral: "#F3F3F3",
+        darkBg: "#0B1036",
+        midBg: "#0E546A",
+        lightBg: "#18B69A",
+      },
+    },
+    fontFamily: {
+      primary: "anona",
+      text: "puritan",
+    },
   },
   plugins: [],
 };
