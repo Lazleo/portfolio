1f061ac87f1aae85f465020391738c187eae4dba
Lazleo <l.belkovits@gmail.com>
2024-06-25 20:22:46 +0200

    add better picture

--- frontend/src/components/Hero/Avatar.jsx
+++ frontend/src/components/Hero/Avatar.jsx
@@ -1,8 +1,9 @@
-
 export default function Avatar() {
   return (
-    <div className="rounded-full w-[300px] shadow-glow">
-      <img src="./img/profile.png" />
+    <div className="absolute top-[30%] left-[75%] -translate-x-[50%] -translate-y-[50%]">
+      <div className="py-10 pl-12 rounded-full bg-gradient-to-bl from-darkBg to-primary shadow-glow h-[350px] w-[350px] overflow-clip">
+        <img className="scale-x-[-1] saturate-0 contrast-125" src="./img/full.png" />
+      </div>
     </div>
   );
 }
