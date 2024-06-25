cbfbd793d636f80afe4674a0565d21517e283e49
Lazleo <l.belkovits@gmail.com>
2024-06-25 17:15:28 +0200

    add svg background, fix overflow

--- frontend/src/components/Experience/Banner.jsx
+++ frontend/src/components/Experience/Banner.jsx
@@ -1,5 +1,7 @@
 export default function Banner() {
   return (
-    <div className="relative flex justify-center items-center h-full w-screen uppercase font-black text-[12rem] text-indigo-900">Experience</div>
-  )
+    <div className="relative flex justify-center items-center h-full w-screen uppercase font-primary font-extrabold text-[12rem] text-indigo-900">
+      Experience
+    </div>
+  );
 }
