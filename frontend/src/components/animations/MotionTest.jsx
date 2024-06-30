ea8e56738342384fd9ea3cbb663988d242034ab7
Lazleo <l.belkovits@gmail.com>
2024-06-30 10:16:26 +0200

    add x transform

--- frontend/src/components/animations/MotionTest.jsx
+++ frontend/src/components/animations/MotionTest.jsx
@@ -1,4 +1,4 @@
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, MotionConfig } from "framer-motion";
 import { useState } from "react";
 
 export default function MotionTest() {
@@ -24,8 +24,8 @@ export default function MotionTest() {
             animate={{
               rotate: "180deg",
               scale: 1,
-              y: [150, -300, 150],
-              x: 150,
+              y: [0, -150, -300, -150, 0],
+              x: [-150, 0, 150, 150, -150],
             }}
             exit={{
               rotate: "0deg",
@@ -36,12 +36,33 @@ export default function MotionTest() {
             transition={{
               duration: 1,
               ease: "backInOut",
-              times: [0, 0.65, 0.85]
+              times: [0, 0.25, 0.65, 0.85, 1],
             }}
             className="w-[150px] h-[150px] bg-primary rounded-xl"
           ></motion.div>
         )}
       </AnimatePresence>
+      <MotionConfig
+        transition={{
+          duration: 0.25,
+          ease: "easeInOut",
+        }}
+      >
+        <motion.button
+          whileHover={{ scale: 1.15, rotate: "2.85deg" }}
+          whileTap={{ backgroundColor: "red" }}
+          className="border-2 rounded-xl bg-primary/50 py-2 px-4 h-fit font-primary text-xl"
+        >
+          click me!
+        </motion.button>
+        <motion.button
+          whileHover={{ scale: 1.15, rotate: "-5deg" }}
+          whileTap={{ backgroundColor: "red" }}
+          className="border-2 rounded-xl bg-accent py-2 px-4 h-fit font-primary text-xl"
+        >
+          click me!
+        </motion.button>
+      </MotionConfig>
     </div>
   );
 }
