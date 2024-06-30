bc4702cfe333201be9b090237967a60989501332
Lazleo <l.belkovits@gmail.com>
2024-06-30 11:22:18 +0200

    add content to project cards

--- frontend/src/routes/sections/Selected.jsx
+++ frontend/src/routes/sections/Selected.jsx
@@ -5,22 +5,34 @@ export default function Selected() {
   const works = [
     {
       title: "worthwhile",
-      description: "eco-friendly savings tracker app",
+      description: {
+        header: "eco-friendly inventory tracker app",
+        body: "small-scale personal investment and maintenance assistant",
+      },
       isOdd: true,
     },
     {
       title: "battleScore",
-      description: "D&D 5e tabletop RPG tool",
+      description: {
+        header: "D&D 5e tabletop RPG tool",
+        body: "combat phase helper tracking initiative order, hp, and other stats or effects",
+      },
       isOdd: false,
     },
     {
       title: "TOTEM",
-      description: "interactive tribute to a kinetic light art installation",
+      description: {
+        header: "interactive 3D web portfolio",
+        body: "portfolio presenting an interactive tribute to the only work of an art installation duo",
+      },
       isOdd: true,
     },
     {
       title: "CineMania",
-      description: "movie ticket vendor webshop",
+      description: {
+        header: "movie ticket vendor webshop",
+        body: "team project within Progmatic Academy FS '23-'24",
+      },
       isOdd: false,
     },
   ];
