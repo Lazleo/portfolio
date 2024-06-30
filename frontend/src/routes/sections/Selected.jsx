19b29529f923819ddfaf0c6d2b8f4161832166ae
Lazleo <l.belkovits@gmail.com>
2024-06-30 10:56:41 +0200

    add content to project cards

--- frontend/src/routes/sections/Selected.jsx
+++ frontend/src/routes/sections/Selected.jsx
@@ -1,18 +1,35 @@
 import ProjectCard from "../../components/Selected/ProjectCard";
+// TODO: automate mapping odd and pair cards
 
 export default function Selected() {
   const works = [
     {
       title: "worthwhile",
       description: "eco-friendly savings tracker app",
+      isOdd: true,
+    },
+    {
+      title: "battleScore",
+      description: "D&D 5e tabletop RPG tool",
+      isOdd: false,
+    },
+    {
+      title: "TOTEM",
+      description: "interactive tribute to a kinetic light art installation",
+      isOdd: true,
+    },
+    {
+      title: "CineMania",
+      description: "movie ticket vendor webshop",
+      isOdd: false,
     },
   ];
 
   return (
     <div className="flex flex-col items-center">
-      {works.map((work) => (
-        <ProjectCard card={work} key={work} />
-      ))}
+      {works.map((work) => {
+        return <ProjectCard card={work} key={work} />;
+      })}
     </div>
   );
 }
