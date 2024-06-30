8d44f6542a740be0db537d25fba7ffed3bd2ea3c
Lazleo <l.belkovits@gmail.com>
2024-06-30 08:34:07 +0200

    add basics-of-motion test

--- frontend/src/components/animations/MotionTest.jsx
+++ frontend/src/components/animations/MotionTest.jsx
@@ -1,7 +1,47 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+
 export default function MotionTest() {
+  const [isVisible, setIsVisible] = useState(true);
   return (
-    <div className="border-2 flex justify-center py-10 m-5 rounded-xl">
-        
+    <div className="border-2 flex flex-col justify-center items-center gap-4 py-[150px] m-5 rounded-xl">
+      <motion.button
+        onClick={() => setIsVisible(!isVisible)}
+        className="border-2 rounded-xl bg-accent py-2 px-4 h-fit font-primary text-xl"
+        layout
+      >
+        Show/Hide
+      </motion.button>
+      <AnimatePresence mode="popLayout">
+        {isVisible && (
+          <motion.div
+            initial={{
+              rotate: "0deg",
+              scale: 0,
+              y: 0,
+              x: -150,
+            }}
+            animate={{
+              rotate: "180deg",
+              scale: 1,
+              y: [150, -300, 150],
+              x: 150,
+            }}
+            exit={{
+              rotate: "0deg",
+              scale: 0,
+              y: 0,
+              x: -150,
+            }}
+            transition={{
+              duration: 1,
+              ease: "backInOut",
+              times: [0, 0.65, 0.85]
+            }}
+            className="w-[150px] h-[150px] bg-primary rounded-xl"
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </div>
-  )
+  );
 }
