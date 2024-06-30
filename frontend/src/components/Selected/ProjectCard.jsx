950d6e655b4bb02e54b6e8178995ba6f0dd5edc4
Lazleo <l.belkovits@gmail.com>
2024-06-30 10:39:05 +0200

    add project card layout

--- frontend/src/components/Selected/ProjectCard.jsx
+++ frontend/src/components/Selected/ProjectCard.jsx
@@ -1,5 +1,22 @@
+import PrimaryButton from "../PrimaryButton";
+import SecondaryButton from "../SecondaryButton";
+
 /* eslint-disable react/prop-types */
 export default function ProjectCard(props) {
   const { card } = props;
-  return <div className="border-2 w-[45vw] h-[200px] rounded-3xl my-12 flex justify-center items-center text-4xl text-slate-200 font-black">{card}</div>;
+  return (
+    <div className="border-2 w-fit h-fit rounded-3xl my-12 flex p-8">
+      <div className="w-[200px] border-2"></div>
+      <div className="flex flex-col justify-center items-center font-primary gap-2 ml-16">
+        <div className="flex flex-col">
+          <div className="text-4xl font-extrabold tracking-wider">{card.title}</div>
+          <div className="text-sm">{card.description}</div>
+        </div>
+        <div className="flex flex-col gap-2 mt-4">
+          <SecondaryButton text={`view code`} size={"lg"} />
+          <PrimaryButton text={`let's collaborate!`} size={"lg"} />
+        </div>
+      </div>
+    </div>
+  );
 }
