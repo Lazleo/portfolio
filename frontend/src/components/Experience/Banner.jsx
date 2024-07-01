e1c1f691ba46a136ef5f1221844f5096e0e8ff18
Lazleo <l.belkovits@gmail.com>
2024-07-01 09:08:57 +0200

    add infinity carousel animation

--- frontend/src/components/Experience/Banner.jsx
+++ frontend/src/components/Experience/Banner.jsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <div className="flex justify-center items-center h-full w-screen uppercase font-primary text-[15rem] text-accent">
+    <div className="flex justify-center items-center h-full w-screen uppercase tracking-widest font-primary font-extrabold text-[15rem] text-accent">
       Experience
     </div>
   );
--- frontend/src/routes/sections/Experience.jsx
+++ frontend/src/routes/sections/Experience.jsx
@@ -1,5 +1,8 @@
+import { useEffect, useRef } from "react";
 import Banner from "../../components/Experience/Banner";
 import XPCard from "../../components/Experience/XPCard";
+import { animate, motion, useMotionValue, useScroll } from "framer-motion";
+import useMeasure from "react-use-measure";
 
 export default function Experience() {
   const cards = [
@@ -28,16 +31,52 @@ export default function Experience() {
       date: "2023 - 2024",
     },
   ];
+
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start end", "end start"],
+  });
+
+  let [ref, { width }] = useMeasure();
+
+  const xTranslation = useMotionValue(scrollYProgress);
+
+  useEffect(() => {
+    let controls;
+    let finalposition = -width / 2 - 4;
+
+    controls = animate(xTranslation, [0, finalposition], {
+      ease: "linear",
+      duration: 25,
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 0,
+    });
+
+    return controls.stop;
+  }, [xTranslation, width]);
+
   return (
     <div id="experience" className="flex flex-col justify-center relative">
       <div className="w-screen h-[300px] bg-lightBg">
         <Banner />
       </div>
-      <div className="flex absolute">
-        {cards.map((card) => (
-          <XPCard title={card.title} body={card.body} subTitle={card.subTitle} date={card.date} key={card} />
+      <motion.div
+        ref={ref}
+        className="flex absolute"
+        style={{ x: xTranslation }}
+      >
+        {[...cards, ...cards].map((card) => (
+          <XPCard
+            title={card.title}
+            body={card.body}
+            subTitle={card.subTitle}
+            date={card.date}
+            key={card}
+          />
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 }
