4e16cc0c0c86bc0eb2498776768d7228c2f9a46b
Lazleo <l.belkovits@gmail.com>
2024-07-02 14:00:51 +0200

    add buttons pointer style

--- frontend/src/components/PrimaryButton.jsx
+++ frontend/src/components/PrimaryButton.jsx
@@ -2,6 +2,6 @@
 export default function PrimaryButton(props) {
     const {text, size} = props
   return (
-    <div className={`font-primary font-extrabold text-accent text-${size} bg-primary rounded-full w-full text-center py-2 px-8`}>{text}</div>
+    <div className={`font-primary font-extrabold text-accent text-${size} bg-primary hover:shadow-glow-sm hover:cursor-pointer active:bg-lightBg active:shadow-none rounded-full w-full text-center py-2 px-8`}>{text}</div>
   )
 }
--- frontend/src/components/SecondaryButton.jsx
+++ frontend/src/components/SecondaryButton.jsx
@@ -2,6 +2,10 @@
 export default function SecondaryButton(props) {
     const {text, size} = props
   return (
-    <div className={`font-primary border-4 border-primary text-neutral text-${size} bg-transparent rounded-full w-full text-center py-2 px-8`}>{text}</div>
-  )
+    <div
+      className={`font-primary border-4 border-primary hover:shadow-glow-sm hover:cursor-pointer active:border-lightBg active:shadow-none text-neutral text-${size} bg-transparent rounded-full w-full text-center py-2 px-8`}
+    >
+      {text}
+    </div>
+  );
 }
